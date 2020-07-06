3c2afc82eb31873b46a55be66dedce81dabcccee
Emmanuel F. Werr <47487994+emmanuelfwerr@users.noreply.github.com>
2020-07-06 15:42:18 -0400

    Update sql_queries.py
    Add DROP TABLES, CREATE TABLES, and INSERT INTO TABLES

--- Data Modeling with PostgreSQL/sql_queries.py
+++ Data Modeling with PostgreSQL/sql_queries.py
@@ -1,44 +1,54 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (songplay_id int, start_time timestamp, user_id int, level char(1), song_id varchar, artist_id varchar, session_id int, location, user_agent)
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (user_id varchar, first_name varchar, last_name varchar, gender char(1), level char(1))
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar, artist_id varchar, year int, duration numeral)
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (artist_id varchar, name varchar, location SRID, latitude lat, longitude long)
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (start_time timestamp, hour int, day int, week int, month int, year int, weekday int)
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
 """)
 
 user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level)
 """)
 
 song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration)
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
 """)
 
 
 time_table_insert = ("""
+INSERT INTO time (start_time, hour, day, week, month, year, weekday)
 """)
 
 # FIND SONGS
