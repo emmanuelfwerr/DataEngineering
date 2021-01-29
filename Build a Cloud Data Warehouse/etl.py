38d28950899084fe11ba8889b535f47219b2e866
Emmanuel F. Werr <47487994+emmanuelfwerr@users.noreply.github.com>
2021-01-28 23:09:04 -0400

    Update etl.py
    add function docstrings

--- Build a Cloud Data Warehouse/etl.py
+++ Build a Cloud Data Warehouse/etl.py
@@ -4,12 +4,26 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Load staging tables.
+    
+    Keyword arguments:
+    cur  -- cursor
+    conn -- connection to data base
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Insert data into tables.
+    
+    Keyword arguments:
+    cur  -- cursor
+    conn -- connection to data base
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
@@ -19,14 +33,17 @@ def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    cur = conn.cursor()
-    
-    load_staging_tables(cur, conn)
-    insert_tables(cur, conn)
+    try:
+        conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+        cur = conn.cursor()
 
-    conn.close()
+        load_staging_tables(cur, conn)
+        insert_tables(cur, conn)
 
+        conn.close()
+        
+    except Exception as error:
+        print(error)
 
 if __name__ == "__main__":
     main()
